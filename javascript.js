61f87be15127f01088fa25e9b7e313aae3f9ed60
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-01-21 21:11:59 -0500

    Resume project

--- javascript.js
+++ javascript.js
@@ -3,42 +3,6 @@ const gameBoard = (function () {
     const columns = 3;
     let board = [];
 
-    let test = 0;
-    let testArray = [1, 2, 3, 4, 5];
-    let testNestedArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-
-    function testInc() {
-        test++;
-    }
-
-    function testSet(a) {
-        test = a;
-    }
-    
-    function currentTest() {
-        return test;
-    }
-
-    function resetTest() {
-        test = 0;
-    }
-
-    function setTestArray(a) {
-        testArray[3] = a;
-    }
-
-    function currentTestArray() {
-        return testArray;
-    }
-
-    function currentTestNestedArray() {
-        return testNestedArray;
-    }
-
-    function setTestNestedArray() {
-        testNestedArray[1][1] = 20;
-    }
-
     for (let i = 0; i < rows; i++){
         board[i] = [];
         for (let j = 0; j < columns; j++){
@@ -63,37 +27,11 @@ const gameBoard = (function () {
         return board;
     }
 
-    return {update, reset, currentBoard, testInc, currentTest, resetTest, testSet, setTestArray, currentTestArray, currentTestNestedArray, setTestNestedArray};
+    return {update, reset, currentBoard};
 })();
 
-console.log("Actual Project");
-console.log(gameBoard.currentBoard());
+console.log(JSON.stringify(gameBoard.currentBoard()));
 gameBoard.update(1, 2, 1);
-console.log(gameBoard.currentBoard());
+console.log(JSON.stringify(gameBoard.currentBoard()));
 gameBoard.reset();
-console.log(gameBoard.currentBoard());
-
-console.log("Single variable test");
-console.log(gameBoard.currentTest());
-gameBoard.testInc();
-console.log(gameBoard.currentTest());
-gameBoard.testInc();
-console.log(gameBoard.currentTest());
-gameBoard.testInc();
-console.log(gameBoard.currentTest());
-gameBoard.resetTest();
-console.log(gameBoard.currentTest());
-gameBoard.testInc();
-console.log(gameBoard.currentTest());
-gameBoard.testSet(5);
-console.log(gameBoard.currentTest());
-
-console.log("Single array test");
-console.log(gameBoard.currentTestArray());
-gameBoard.setTestArray(9);
-console.log(gameBoard.currentTestArray());
-
-console.log("Nested array test");
-console.log(gameBoard.currentTestNestedArray());
-gameBoard.setTestNestedArray();
-console.log(gameBoard.currentTestNestedArray());
+console.log(JSON.stringify(gameBoard.currentBoard()));
